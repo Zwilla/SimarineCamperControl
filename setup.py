1bb00d51451c9ebacf6b71b7bcf38030b102b67e
Zwilla <info@zwilla.de>
2023-04-09 22:15:42 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='SimarineReader',
+    version='1.0 pre beta',
+    packages=[''],
+    url='https://github.com/zwilla/SimarineCamperControl',
+    baseRepository="https://github.com/htool/pico2signalk",
+    license='MIT',
+    author="Htool Hans",
+    coauthor='Michael Padilla',
+    coauthor_email='esmipa@zwilla.de',
+    description='Simarine Data Reader'
+)
